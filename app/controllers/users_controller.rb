ba2363d904c13c93e4837e77942d35933a638c80
mihe.cbx <mihe.cbx@taobao.com>
2016-01-28 12:55:54 +0800

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the G House"
       redirect_to @user
-      flash[:success] = "Welcome to the Sample App!"
     else
       render 'new'
     end
